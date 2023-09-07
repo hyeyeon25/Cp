0fb868a200568ddecb5c907ce65bf5d4cdd11f14
hyeyeon25 <hyb25@smail.kongju.ac.kr>
2023-09-07 10:44:15 +0900

    pyramid

---
+++ Project11/Project11/forif.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+	int floor = 0;
+	printf("¸î ÃþÀ¸·Î ½×°Ú½À´Ï±î?");
+	scanf_s("%d", &floor);
+
+	for (int i = 0; i < floor; i++)
+	{
+		for (int j = i; j < floor - 1; j++)
+		{
+			printf(" ");
+		}
+
+		for (int k = 0; k < i * 2 + 1; k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
+
+	return 0;
+}
