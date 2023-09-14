4e1608a60c84b7efa34b5eaaf670878e3d394d8d
hyeyeon25 <hyb25@smail.kongju.ac.kr>
2023-09-14 12:05:38 +0900

    구구단

---
+++ Project1/Project1/lecture10.c
@@ -0,0 +1,18 @@
+// 구구단 2~9단 출력
+// 5단은 출력 x
+
+#include <stdio.h>
+
+int main()
+{
+	int i, j;
+	
+	for(i = 2; i <= 9; i++){
+		if(i == 5) continue;
+		for(j = 1; j <= 9; j++){
+			printf("%d * %d = %d\n", i, j, i * j);
+		}
+	}	
+	
+	return 0;
+}
