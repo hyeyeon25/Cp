e9903f33eddcd8a0a348f3fc5910153e556832e1
hyeyeon25 <hyb25@smail.kongju.ac.kr>
2023-09-14 10:57:47 +0900

    year

--- Project1/Project1/lecture5.c
+++ Project1/Project1/lecture5.c
@@ -6,10 +6,12 @@ int main()
 	printf("연도를 입력하세요. : ");
 	scanf("%d", year);
 	
-	if(year % 100 != 0) && (year % 4 == 0) && (year % 400 == 0);
+	int y = (year % 100 != 0) && (year % 4 == 0) && (year % 400 == 0);
+	
+	if (y)
 	printf("%d년은 윤년입니다.\n", year);
 	
-	if else
+	else
 	printf("%d년은 윤년이 아닙니다.\n", year);
 	
 	return 0; 
