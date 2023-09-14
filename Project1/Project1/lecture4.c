5bd85f3eea2043c6210118f2846585be575950be
hyeyeon25 <hyb25@smail.kongju.ac.kr>
2023-09-14 10:33:53 +0900

    logic

---
+++ Project1/Project1/lecture4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	int a, b, c, val1, val2, val3, val4, val5;
+	a = 0, b = 5, c = 8;
+	
+	val1 = a && b;
+	val2 = a || c;
+	val3 = (a > b) && (a < c);
+	val4 = !val3;
+	val5 = b && c;
+	
+	printf("a = 0, b = 5, c = 8ÀÏ ¶§\n");
+	printf("1. a && b = %d", &val1);
+	printf("2. a || c = %d", &val2);
+	printf("3. (a > b) && (a < c) = %d", val3);
+	printf("4. !val3 = %d", val4);
+	printf("5. b && c = %d", val5);
+	
+	return 0;
+}
