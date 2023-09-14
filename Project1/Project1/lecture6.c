bdb52d123e29af49d8a17567a65a254b36671413
hyeyeon25 <hyb25@smail.kongju.ac.kr>
2023-09-14 11:42:59 +0900

    양/음/0

---
+++ Project1/Project1/lecture6.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	
+	scanf("%d", &a);
+	
+	if(a > 0)
+	printf("양의 정수입니다.\n");
+	
+	else if(a == 0)
+	printf("0입니다.\n");
+	
+	else
+	printf("음의 정수입니다.\n");
+	
+	return 0;	
+}
+
+// 양수 음수 0 구분 프로그램 
