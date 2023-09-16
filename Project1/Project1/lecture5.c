dcc2f936daaee69db87ddbcc3d3e283bc5be434d
hyeyeon25 <hyb25@smail.kongju.ac.kr>
2023-09-16 20:47:23 +0900

    year

--- Project1/Project1/lecture5.c
+++ Project1/Project1/lecture5.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
 	int year;
 	printf("연도를 입력하세요. : ");
-	scanf("%d", year);
+	scanf("%d", &year);
 	
-	int y = (year % 100 != 0) && (year % 4 == 0) && (year % 400 == 0);
+//	int y = (year % 100 != 0) && (year % 4 == 0) && (year % 400 != 0);
+//	if (y)
 	
-	if (y)
-	printf("%d년은 윤년입니다.\n", year);
+	if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) 
+		printf("%d년은 윤년입니다.\n", year);
 	
 	else
-	printf("%d년은 윤년이 아닙니다.\n", year);
+		printf("%d년은 윤년이 아닙니다.\n", year);
 	
-	return 0; 
+	return 0;
 }
+
+
+&& and
+|| or
