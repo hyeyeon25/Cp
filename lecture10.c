ca2fda7878787c4cd1714054c053e690032c72a3
hyeyeon25 <hyb25@smail.kongju.ac.kr>
2023-09-16 21:59:49 +0900

    구구단

--- lecture10.c
+++ lecture10.c
@@ -5,14 +5,14 @@
 
 int main()
 {
-	int i, j
+	int i, j;
 	
 	for(i = 2; i <= 9; i++){
 		if(i == 5) continue;
 		for(j = 1; j <= 9; j++){
 			printf("%d * %d = %d\n", i, j, i * j);
 		}
-	}	
+	}
 	
 	return 0;
 }
