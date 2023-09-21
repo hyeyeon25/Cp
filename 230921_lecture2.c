0948c4460704a690a3cbbb35f11dd5cb5ad01207
hyeyeon25 <hyb25@smail.kongju.ac.kr>
2023-09-21 10:06:08 +0900

    230921_lecture2

--- 230921_lecture2.c
+++ 230921_lecture2.c
@@ -1,26 +1,35 @@
 #include <stdio.h>
 
-int main()
+void calculator(int selector,int num1, int num2);
+
+void sum(double a, double b)
 {
-	int sel;
-	
-	printf("1.더하기 2.빼기 3.곱하기 4.나누기 : ");
-	scanf("%d", &sel);
-	
-	calculator(sel);
-	
-	return 0;
+	double result = a + b;
+	printf("%lf + %lf : %lf", a, b, result);
 }
 
-void calculator(int selector)
+void sub(double a, double b)
 {
+	double result = a - b;
+	printf("%lf - %lf : %lf", a, b, result);
+}
+
+void mul(double a, double b)
+{
+	double result = a * b;
+	printf("%lf * %lf : %lf", a, b, result);
+}
+
+void div(double a, double b)
+{
+	double result = a / b;
+	printf("%lf / %lf : %lf", a, b, result);
+}
+
+int main()
+{
+	int selector;
 	double num1, num2;
-	
-	if(selector < 1 || selector > 4)
-	{
-		printf("오류!!");
-		return;
-	}
 	
 	printf("숫자 1을 입력하시오 : ");
 	scanf("%lf", &num1);
@@ -28,9 +37,27 @@ void calculator(int selector)
 	printf("숫자 2를 입력하시오 : ");
 	scanf("%lf", &num2);
 	
+	
+	printf("1.더하기 2.빼기 3.곱하기 4.나누기 : ");
+	scanf("%d", &selector);
+	
+	calculator(selector, num1, num2);
+	
+	return 0;
+}
+
+void calculator(int selector, int num1, int num2)
+{
+
+	if(selector < 1 || selector > 4)
+	{
+		printf("오류!!");
+		return;
+	}
+	
 	if (selector == 1) sum(num1, num2);
-	else if (selector == 2) sum(num1, num2);
-	else if (selector == 3) sum(num1, num2);
-	else if (selector == 4) sum(num1, num2);
+	else if (selector == 2) sub(num1, num2);
+	else if (selector == 3) mul(num1, num2);
+	else if (selector == 4) div(num1, num2);
 
 }
