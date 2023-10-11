91973446db1a9a0f164037d3e42928326cbd462f
hyeyeon25 <hyb25@smail.kongju.ac.kr>
2023-10-11 22:40:47 +0900

    Update 230921_lecture4.c

--- 230921/230921_lecture4.c
+++ 230921/230921_lecture4.c
@@ -4,34 +4,34 @@
 
 int main() {
     int points = 1000000000;
-    int circle = 0;  
+    int circle = 0;
 
     srand(time(NULL));
 
     for (int i = 0; i <= points; i++) {
         double x = (double)rand() / RAND_MAX;
         double y = (double)rand() / RAND_MAX;
-		//랜덤하게 다트 던짐
-        double distance = x * x + y * y;
+        
+    double distance = x * x + y * y;
 
-        if (distance <= 1) {
-            circle++;
-        }
+    if (distance <= 1) {
+        circle++;
+    	}
 
-        if (i % 10000000 == 0) {
-            double pi = 4.0 * circle / (i + 1); 
-            double percent = (double)(i + 1) / points * 100.0;
-            printf("%2.f%% 진행.. 원주율 : %f \n", percent, pi);
-            for (int k = 1; k <= 20; k++) {
-                if (k <= percent / 5) {
-                    printf("■");
-                }
-                else 
-                    printf("□");
-                
-            }
+    if (i % 10000000 == 0) {
+        double pi = 4.0 * circle / (i + 1);
+        double percent = (double)(i + 1) / points * 100.0;
+        printf("%2.f%% 진행.. 원주율 : %f \n", percent, pi);
+        for (int k = 1; k <= 20; k++) {
+            if (k <= percent / 5) {
+                printf("■");
+            	}
+            else 
+                printf("□");
+        		}
+        	
             printf("\n");
-        }
+        	}
     }
 
     double pi = 4.0 * circle / points;
@@ -42,4 +42,4 @@ int main() {
     return 0;
 }
 
-//%(전체 자릿수).0(소수점 이하 미표시)f(progress)%(이거 하나는 뭐지?)%(찐 퍼센트)
+//%(전체 자릿수).0(소수점 이하 미표시)f(progress)%(퍼센트 표시)%(이거 하나는 뭐지? 근데 얘 지우면 결과값에 %가 없음)
