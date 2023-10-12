7cc0309485e4d7b6224b81c1e60ff8c99af317e2
hyeyeon25 <hyb25@smail.kongju.ac.kr>
2023-10-12 10:44:50 +0900

    Update 231012_lecture4.c

--- 231012/231012_lecture4.c
+++ 231012/231012_lecture4.c
@@ -5,9 +5,9 @@ int main()
 	int score[5] = {87, 92, 94, 65, 33};
 	int cnt;
 	
-	int* p_score = score + 1;
+	int* p_score = score;
 	
-	for (cnt = 0; cnt < 3; cnt++) {
+	for (cnt = 0; cnt < 5; cnt++) {
 		printf("%d\n", score[cnt]);
 	}
 	return 0;
