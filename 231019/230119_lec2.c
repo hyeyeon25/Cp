5b9c1b24e740eed1d2cee1fdae40bb33d78bc99a
hyeyeon25 <hyb25@smail.kongju.ac.kr>
2023-10-26 03:30:36 +0900

    Update 230119_lec2.c

--- 231019/230119_lec2.c
+++ 231019/230119_lec2.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef struct _NODE {
+typedef struct _NODE { // 연결 리스트의 노드 나타냄 
    int data;
    struct _NODE* next;
 } NODE;
 
-void print_list(NODE* head)
+void print_list(NODE* head) // 실제 데이터 포함 x, 연결리스트의 시작 알림 
 {
    NODE* p = head->next;
    while (p != NULL) {
@@ -15,7 +15,7 @@ void print_list(NODE* head)
    }
    printf("\n");
 }
-void insert_node(NODE* head, int data)
+void insert_node(NODE* head, int data) // 연결리스트 끝에 새로운 노드 추가 
 {
    NODE* p = head;
    while (p->next != NULL) {
@@ -26,7 +26,7 @@ void insert_node(NODE* head, int data)
    new_node->next = p->next;
    p->next = new_node;
 }
-void insert_node_first(NODE* head, int data)
+void insert_node_first(NODE* head, int data) // 연결리스트의 첫 번째에 새로운 노드 추가 
 {
    NODE* new_node = (NODE*)malloc(sizeof(NODE));
    new_node->data = data;
@@ -54,6 +54,7 @@ int main()
 {
    NODE* head = (NODE*)malloc(sizeof(NODE));
    head->next = NULL;
+   
    NODE* n1 = (NODE*)malloc(sizeof(NODE));
    n1->data = 1;
    n1->next = head->next;
@@ -71,7 +72,7 @@ int main()
 
    print_list(head);
 
-   insert_node(head, 4);
+   insert_node(head, 4); // 리스트 끝에 추가 
    insert_node(head, 5);
 
    print_list(head);
@@ -79,11 +80,11 @@ int main()
    insert_node_first(head,6);
    print_list(head);
 
-   delete_node_first(head);
-   delete_node_first(head);
+   delete_node_first(head); // 첫 번째 노드 제거 
+   delete_node_first(head); // 두 번째 노드 제거 
    print_list(head);
 
-   delete_node_last(head);
+   delete_node_last(head); // 마지막 노드 제거 
    print_list(head);
    return 0;
 }
