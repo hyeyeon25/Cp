d45a1de75fafa9be89779fb25dbda8be9e54f321
hyeyeon25 <hyb25@smail.kongju.ac.kr>
2023-10-26 03:49:02 +0900

    Update 231019_lec1.c

--- 231019/231019_lec1.c
+++ 231019/231019_lec1.c
@@ -1,44 +1,3 @@
-/*
-#include <stdio.h>
-#include <stdlib.h>
-
-struct Student {
-	int sno;
-	char name[10];
-	int score;
-};
-
-int main() {
-	int std;
-	struct Students* s;
-	printf("학생수 입력");
-	scanf("%d", &std);
-	scores = (int *)malloc(std * sizeof(struct Student));
-	
-	for(int i = 0; i < std; i++) {
-		printf("학생 # %d-%d 학번 입력", std, i + 1);
-		scanf("%d", %s[i].sno);
-		//scanf("%d", &scores[i]);
-		//scanf("%d", scores + 1)
-		printf("학생 # %d-%d 이름 입력", std, i + 1);
-		scanf("%s", s[i].name, 10);	//이건 왜 & 안 씀? => 문자열이라 
-		printf("학생 # %d-%d 성적 입력", std, i + 1);
-		scanf("%d", %s[i].score);
-	};
-	printf("총점: %d\n", sum)
-	
-	int sum = 0;
-	for (int i = 0; i < std; i++) {
-		sum += s[i].score;
-	}
-	printf("%d\n", sum);
-
-	free(scores);
-	
-	return 0;
-}
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -81,3 +40,44 @@ int main() {
    return 0;
 
 }
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Student {
+	int sno;
+	char name[10];
+	int score;
+};
+
+int main() {
+	int std;
+	struct Students* s;
+	printf("학생수 입력");
+	scanf("%d", &std);
+	scores = (int *)malloc(std * sizeof(struct Student));
+	
+	for(int i = 0; i < std; i++) {
+		printf("학생 # %d-%d 학번 입력", std, i + 1);
+		scanf("%d", %s[i].sno);
+		//scanf("%d", &scores[i]);
+		//scanf("%d", scores + 1)
+		printf("학생 # %d-%d 이름 입력", std, i + 1);
+		scanf("%s", s[i].name, 10);	//이건 왜 & 안 씀? => 문자열이라 
+		printf("학생 # %d-%d 성적 입력", std, i + 1);
+		scanf("%d", %s[i].score);
+	};
+	printf("총점: %d\n", sum)
+	
+	int sum = 0;
+	for (int i = 0; i < std; i++) {
+		sum += s[i].score;
+	}
+	printf("%d\n", sum);
+
+	free(scores);
+	
+	return 0;
+}
+*/
